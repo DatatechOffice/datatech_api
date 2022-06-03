af10fff1a113a596ca55af6ef6fdfed079f0e8ee
Vitor R. Lima <56142288+VilRL@users.noreply.github.com>
2022-06-02 22:40:26 -0300

    Add files via upload

--- DomRockApi/src/main/java/layout/Bronze.java
+++ DomRockApi/src/main/java/layout/Bronze.java
@@ -1,51 +1,60 @@
 package layout;
 
 import java.awt.EventQueue;
-import java.awt.Font;
 
 import javax.swing.JFrame;
+import java.awt.SystemColor;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 
-import DAO.DaoCliente;
-import DAO.DaoCliente_Dados;
-import DAO.DaoCliente_Produto;
-import DAO.DaoTipo_Dados;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JCheckBox;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextPane;
+
 import controlador.Cliente;
 import controlador.Dados;
-
-import javax.swing.JComboBox;
-import javax.swing.JSpinner;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
-import javax.swing.JList;
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Bronze {
 
 	JFrame frmBronze;
 	Cliente c1;
 	EscopoInsert vProList;
-	private JTextField textField_vEnvio;
-	private String vTipo;
-	private Dados dados;
+	private JTextField textField_vQuantidade;
+	
+
 	/**
 	 * Launch the application.
 	 */
-	
-	
-	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Bronze window = new Bronze();
+					window.frmBronze.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 	/**
 	 * Create the application.
 	 */
-	
-	public Bronze(Dados dados) {
-		this.dados=dados;
+	public Bronze() {
+		initialize();
 	}
+	
 	public Bronze(Cliente c1) {
 		this.c1=c1;
 		initialize();
@@ -57,212 +66,198 @@ public class Bronze {
 	private void initialize() {
 		frmBronze = new JFrame();
 		frmBronze.setTitle("Bronze");
-		frmBronze.setBounds(100, 100, 631, 331);
-		frmBronze.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmBronze.getContentPane().setBackground(SystemColor.windowBorder);
 		frmBronze.getContentPane().setLayout(null);
-		frmBronze.setLocationRelativeTo(null);
 		
-		JLabel lblNewLabel = new JLabel("Tipo de Dado");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel.setBounds(10, 11, 141, 14);
+		JLabel lblNewLabel = new JLabel("Tipos de Dados:");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 20));
+		lblNewLabel.setBounds(10, 10, 200, 31);
 		frmBronze.getContentPane().add(lblNewLabel);
 		
-		JLabel lblFormaDeEnvio = new JLabel("Forma de Envio");
-		lblFormaDeEnvio.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblFormaDeEnvio.setBounds(10, 96, 141, 14);
-		frmBronze.getContentPane().add(lblFormaDeEnvio);
-		
-		JLabel lblQuantidadeRecebida = new JLabel("Prazo deDados Recebida");
-		lblQuantidadeRecebida.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblQuantidadeRecebida.setBounds(10, 155, 246, 14);
-		frmBronze.getContentPane().add(lblQuantidadeRecebida);
-		
-		JButton btnNewButton = new JButton("Prosseguir");
-
-		btnNewButton.setBounds(10, 258, 141, 23);
-		frmBronze.getContentPane().add(btnNewButton);
-		
-		final JCheckBox vDados_PDF = new JCheckBox("PDF");
-		vDados_PDF.setBounds(14, 32, 97, 23);
+		JCheckBox vDados_PDF = new JCheckBox("PDF");
+		vDados_PDF.setForeground(Color.WHITE);
+		vDados_PDF.setFont(new Font("Arial", Font.BOLD, 15));
+		vDados_PDF.setBackground(SystemColor.windowBorder);
+		vDados_PDF.setBounds(10, 47, 150, 31);
 		frmBronze.getContentPane().add(vDados_PDF);
 		
-		final JCheckBox vDados_JSON = new JCheckBox("JSON");
-		vDados_JSON.setBounds(113, 32, 97, 23);
+		JCheckBox vDados_JSON = new JCheckBox("JSON");
+		vDados_JSON.setForeground(Color.WHITE);
+		vDados_JSON.setFont(new Font("Arial", Font.BOLD, 15));
+		vDados_JSON.setBackground(SystemColor.windowBorder);
+		vDados_JSON.setBounds(162, 47, 150, 31);
 		frmBronze.getContentPane().add(vDados_JSON);
 		
-		final JCheckBox vDados_CSV = new JCheckBox("CSV");
-		vDados_CSV.setBounds(212, 32, 97, 23);
+		JCheckBox vDados_CSV = new JCheckBox("CSV");
+		vDados_CSV.setForeground(Color.WHITE);
+		vDados_CSV.setFont(new Font("Arial", Font.BOLD, 15));
+		vDados_CSV.setBackground(SystemColor.windowBorder);
+		vDados_CSV.setBounds(314, 47, 150, 31);
 		frmBronze.getContentPane().add(vDados_CSV);
 		
-		final JCheckBox vDados_Planilhas = new JCheckBox("Planilhas");
-		vDados_Planilhas.setBounds(311, 32, 97, 23);
+		JCheckBox vDados_Planilhas = new JCheckBox("Planilhas");
+		vDados_Planilhas.setForeground(Color.WHITE);
+		vDados_Planilhas.setFont(new Font("Arial", Font.BOLD, 15));
+		vDados_Planilhas.setBackground(SystemColor.windowBorder);
+		vDados_Planilhas.setBounds(466, 47, 150, 31);
 		frmBronze.getContentPane().add(vDados_Planilhas);
 		
-		final JCheckBox vDados_Tabelas = new JCheckBox("Tabelas");
-		vDados_Tabelas.setBounds(14, 58, 97, 23);
+		JCheckBox vDados_Tabelas = new JCheckBox("Tabelas");
+		vDados_Tabelas.setForeground(Color.WHITE);
+		vDados_Tabelas.setFont(new Font("Arial", Font.BOLD, 15));
+		vDados_Tabelas.setBackground(SystemColor.windowBorder);
+		vDados_Tabelas.setBounds(10, 80, 150, 31);
 		frmBronze.getContentPane().add(vDados_Tabelas);
 		
-		final JCheckBox vDados_Audio = new JCheckBox("Audio");
-		vDados_Audio.setBounds(113, 58, 97, 23);
+		JCheckBox vDados_Audio = new JCheckBox("Audio");
+		vDados_Audio.setForeground(Color.WHITE);
+		vDados_Audio.setFont(new Font("Arial", Font.BOLD, 15));
+		vDados_Audio.setBackground(SystemColor.windowBorder);
+		vDados_Audio.setBounds(162, 80, 150, 31);
 		frmBronze.getContentPane().add(vDados_Audio);
 		
-		final JCheckBox vDados_Texto = new JCheckBox("vDados_Texto");
-		vDados_Texto.setBounds(212, 58, 97, 23);
+		JCheckBox vDados_Texto = new JCheckBox("Texto");
+		vDados_Texto.setForeground(Color.WHITE);
+		vDados_Texto.setFont(new Font("Arial", Font.BOLD, 15));
+		vDados_Texto.setBackground(SystemColor.windowBorder);
+		vDados_Texto.setBounds(314, 80, 150, 31);
 		frmBronze.getContentPane().add(vDados_Texto);
 		
-		final JCheckBox vOrigem_API = new JCheckBox("API");
-		vOrigem_API.setBounds(6, 117, 97, 23);
-		frmBronze.getContentPane().add(vOrigem_API);
+		JLabel lblFormasDeEnvio = new JLabel("Formas de Envio:");
+		lblFormasDeEnvio.setForeground(Color.WHITE);
+		lblFormasDeEnvio.setFont(new Font("Arial", Font.BOLD, 20));
+		lblFormasDeEnvio.setBounds(10, 117, 200, 31);
+		frmBronze.getContentPane().add(lblFormasDeEnvio);
 		
-		final JCheckBox vOrigem_SFTP = new JCheckBox("SFTP");
-		vOrigem_SFTP.setBounds(105, 117, 97, 23);
-		frmBronze.getContentPane().add(vOrigem_SFTP);
+		JCheckBox vEnvio_API = new JCheckBox("API");
+		vEnvio_API.setForeground(Color.WHITE);
+		vEnvio_API.setFont(new Font("Arial", Font.BOLD, 15));
+		vEnvio_API.setBackground(SystemColor.windowBorder);
+		vEnvio_API.setBounds(10, 154, 150, 31);
+		frmBronze.getContentPane().add(vEnvio_API);
 		
-		final JCheckBox vOrigem_Upload = new JCheckBox("Upload");
-		vOrigem_Upload.setBounds(204, 117, 97, 23);
-		frmBronze.getContentPane().add(vOrigem_Upload);
+		JCheckBox vEnvio_SFTP = new JCheckBox("SFTP");
+		vEnvio_SFTP.setForeground(Color.WHITE);
+		vEnvio_SFTP.setFont(new Font("Arial", Font.BOLD, 15));
+		vEnvio_SFTP.setBackground(SystemColor.windowBorder);
+		vEnvio_SFTP.setBounds(162, 160, 150, 31);
+		frmBronze.getContentPane().add(vEnvio_SFTP);
 		
-		JButton btnRetorna = new JButton("Retorna");
-		btnRetorna.setBounds(160, 258, 141, 23);
-		frmBronze.getContentPane().add(btnRetorna);
+		JCheckBox vEnvio_Upload = new JCheckBox("Upload");
+		vEnvio_Upload.setForeground(Color.WHITE);
+		vEnvio_Upload.setFont(new Font("Arial", Font.BOLD, 15));
+		vEnvio_Upload.setBackground(SystemColor.windowBorder);
+		vEnvio_Upload.setBounds(314, 160, 150, 31);
+		frmBronze.getContentPane().add(vEnvio_Upload);
 		
-		final JSpinner vContagem = new JSpinner();
-		vContagem.setBounds(10, 175, 44, 20);
+		JLabel lblPrazoDeDados = new JLabel("Prazo de Dados Recebidos");
+		lblPrazoDeDados.setForeground(Color.WHITE);
+		lblPrazoDeDados.setFont(new Font("Arial", Font.BOLD, 20));
+		lblPrazoDeDados.setBounds(10, 191, 292, 31);
+		frmBronze.getContentPane().add(lblPrazoDeDados);
+		
+		JSpinner vContagem = new JSpinner();
+		vContagem.setFont(new Font("Arial", Font.BOLD, 15));
+		vContagem.setBounds(10, 232, 40, 31);
 		frmBronze.getContentPane().add(vContagem);
 		
-		final JComboBox JComboBo_vData = new JComboBox();
-		JComboBo_vData.setModel(new DefaultComboBoxModel(new String[] {"<Selecione uma das op\u00E7\u00F5es>", "Dias", "Semanas", "Meses"}));
-		JComboBo_vData.setBounds(64, 174, 192, 22);
-		frmBronze.getContentPane().add(JComboBo_vData);
+		JComboBox comboBox_vPazo = new JComboBox();
+		comboBox_vPazo.setForeground(Color.WHITE);
+		comboBox_vPazo.setFont(new Font("Arial", Font.BOLD, 15));
+		comboBox_vPazo.setBackground(SystemColor.windowBorder);
+		comboBox_vPazo.setModel(new DefaultComboBoxModel(new String[] {"<Selecione uma das op\u00E7\u00F5es>", "Dias", "Semanas", "Meses"}));
+		comboBox_vPazo.setBounds(60, 232, 252, 31);
+		frmBronze.getContentPane().add(comboBox_vPazo);
 		
-		textField_vEnvio = new JTextField();
-		textField_vEnvio.setBounds(10, 227, 246, 20);
-		frmBronze.getContentPane().add(textField_vEnvio);
-		textField_vEnvio.setColumns(10);
+		JLabel lblQuantidadeRecebida = new JLabel("Quantidade Recebida");
+		lblQuantidadeRecebida.setForeground(Color.WHITE);
+		lblQuantidadeRecebida.setFont(new Font("Arial", Font.BOLD, 20));
+		lblQuantidadeRecebida.setBounds(10, 273, 292, 31);
+		frmBronze.getContentPane().add(lblQuantidadeRecebida);
 		
-		JLabel lblQuantidadeRecebida_1 = new JLabel("Quantidade Recebida");
-		lblQuantidadeRecebida_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblQuantidadeRecebida_1.setBounds(10, 212, 174, 14);
-		frmBronze.getContentPane().add(lblQuantidadeRecebida_1);
-		
-		
-		//botão de prosseguir
-		btnNewButton.addActionListener(new ActionListener() {
-			private Dados dados;
+		JButton btnNewButton_Avancar = new JButton("Avan\u00E7ar");
+		btnNewButton_Avancar.addActionListener(new ActionListener() {
 
+			Dados d1 = new Dados(null, 0, null, null);
+			
 			public void actionPerformed(ActionEvent e) {
-				//armezando tipos de dados
+				
+				//<------------------------------ADICIONANDO OS TIPOS DE DADOS NUMA ARRAY------------------------------>
+
 				ArrayList<String> vDadosList = new ArrayList<String>();
+				
+				if(vDados_PDF.isSelected()) {
+					vDadosList.add("PDF");
+					}
 				
 				if(vDados_JSON.isSelected()) {
 					vDadosList.add("JSON");
-					
 				}
+				
 				if(vDados_CSV.isSelected()) {
 					vDadosList.add("CSV");
-					
 				}
+				
 				if(vDados_Planilhas.isSelected()) {
 					vDadosList.add("Planilhas");
-					
 				}
-				if(vDados_Tabelas.isSelected()) {
-					vDadosList.add("Tabelas");
-					
-				}
-				if(vDados_PDF.isSelected()) {
-					vDadosList.add("PDF");
-					
-				}
+				
 				if(vDados_Audio.isSelected()) {
 					vDadosList.add("Audio");
-					
 				}
+				
+				if(vDados_Tabelas.isSelected()) {
+					vDadosList.add("Tabelas");
+				}
+				
 				if(vDados_Texto.isSelected()) {
-					vDadosList.add("Texto");
-					
-				}
+					vDadosList.add("Texto");				
+					}
+				//<------------------------------FIM DA ARRAY DOS TIPOS DE DADOS------------------------------>
 				
-				String vDadosListStr = "";
-				for (String vDadosList1 : vDadosList) {
-					vDadosListStr+= vDadosList1+",";
-				}
-				
-				
-				//fim do armazenamento de tipos de dados
-				
-				//armazena os tipos de envio
+				//<------------------------------ADICIONANDO AS FORMAS DE ENVIO NUMA ARRAY------------------------------>
 				ArrayList<String> vEnviarList = new ArrayList<String>();
 				
-				if(vOrigem_API.isSelected()) {
+				if(vEnvio_API.isSelected()) {
 					vEnviarList.add("API");
-					
 				}
-				if(vOrigem_SFTP.isSelected()) {
+				
+				if(vEnvio_SFTP.isSelected()) {
 					vEnviarList.add("SFTP");
-					
 				}
-				if(vOrigem_Upload.isSelected()) {
+				if(vEnvio_Upload.isSelected()) {
 					vEnviarList.add("Upload");
-					
 				}
+				//<------------------------------FIM DA FORMA DE ENVIO------------------------------>
 
-				//fim do armazenamento dos tipos de envio
-				
-				//Recebendo da quantidade  de dados
-				
-				dados.setvEnvio(textField_vEnvio.getText());
-				dados.setvTempo(vContagem.getValue()+" "+JComboBo_vData.getSelectedItem());
-				vTipo=vDadosListStr;
-				
-				//Armazenando o prazo de dados recebidos
-				if(vDadosList.size() !=0 &&
+				//<------------------------------RECEBENDO A QUANTIDADE DE DADOS------------------------------>
+				d1.setvEnvio(textField_vQuantidade.getText());
+				d1.setvTempo(vContagem.getValue()+""+comboBox_vPazo.getSelectedItem());
+				//<------------------------------FIM DA QUANTIDADE DE DADOS------------------------------>
+
+				if(vDadosList.size() != 0 &&
 						vEnviarList.size() !=0 &&
-						!textField_vEnvio.getText().equals("") &&
-						!JComboBo_vData.getSelectedItem().equals("<Selecione uma das opções>")) {	
-					
-					/*DaoCliente_Dados daoCD=new DaoCliente_Dados(c1);
-					daoCD.novaCD(dados);*/
-					
-					/*DaoCliente_Produto daoCP=new DaoCliente_Produto();
-					daoCP.novaCP(c1);*/
-					
-				//Mostrando pro cliente os dados salvos ate agora
-				JOptionPane.showMessageDialog(null, "Cliente: "+c1.getvNome_Cliente()+"\n"+
-						"Solução: "+c1.getvSolucao_Cliente()+"\n"+
-						"Objetivo: "+c1.getvObjetivo_Cliente()+"\n"+
-						"Entregas Minimas: "+c1.getvEntregaM_Cliente()+"\n"+
-						"Entregas Possiveis: "+c1.getvEntregaP_Cliente()+"\n"+
-						"Produtos Selecionados:"+"\n"+
-						"Tipo de Dados Selecionado:"+vDadosList+"\n"+
-						"Forma de Envio Selecionado:"+vEnviarList+"\n"+
-						"Programação de envio de dados:"+dados.getvTempo()+"\n"+
-						"Quantidade de Dados Recebido: "+dados.getvEnvio());
-				}else {
-					JOptionPane.showMessageDialog(null,"Seleciona um  prazo valido ou Selecione todos os campos");
+						!textField_vQuantidade.getText().equals("") &&
+						!comboBox_vPazo.getSelectedItem().equals("<Selecione uma das opções>")
+						) {
+					JOptionPane.showMessageDialog(null,"Concluido com Sucesso");
 				}
+				
+
+
+				
 			}
 		});
+		btnNewButton_Avancar.setFont(new Font("Arial", Font.BOLD, 15));
+		btnNewButton_Avancar.setBounds(314, 480, 150, 30);
+		frmBronze.getContentPane().add(btnNewButton_Avancar);
 		
-		//Botão de retorna a pagina anteior
-		btnRetorna.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-					EventQueue.invokeLater(new Runnable() {
-						public void run() {
-							try {
-								EscopoInsert window2 = new EscopoInsert();
-								window2.getFrmEscopoInsert().setVisible(true);
-								frmBronze.setVisible(false);
-								
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}
-					});
-					
-				}
-			
-		});
+		textField_vQuantidade = new JTextField();
+		textField_vQuantidade.setBounds(10, 303, 302, 31);
+		frmBronze.getContentPane().add(textField_vQuantidade);
+		textField_vQuantidade.setColumns(10);
+		frmBronze.setBounds(100, 100, 800, 600);
+		frmBronze.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
